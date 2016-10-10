4c2ecf4459afb704e5c4abdc231800bc4627a9be
antra1608 <antra.purohit1608@gmail.com>
2016-10-11 00:33:57 +0530

    Create Exceptions - String to Integer.java

---
+++ Exceptions - String to Integer.java
@@ -0,0 +1,23 @@
+// Java Code
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try{
+        int i=Integer.parseInt(S);
+        System.out.print(i);
+        }
+        catch(Exception e)
+            {
+            System.out.printf("Bad String");
+        }
+    
+    }
+}
